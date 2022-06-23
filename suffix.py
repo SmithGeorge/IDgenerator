b8a2527afceceb46505aae6c0563ecf8c31cb8bf
htydog <htydog@outlook.com>
2022-06-24 02:39:52 +0800

    reduce codes

--- suffix.py
+++ suffix.py
@@ -1,3 +1,2 @@
 def suffix(ID17):
-	 value = [7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2]
-	 return "10X98765432"[sum(int(ID17[i])*value[i] for i in range(0,len(ID17)))%11]
+	 return "10X98765432"[sum(int(ID17[i])*[7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2][i] for i in range(0,len(ID17)))%11]
