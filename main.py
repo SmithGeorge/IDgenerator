f1d397429da459cbe6c30bc2066c39a0f3c1a2eb
htydog <htydog@outlook.com>
2022-06-23 13:45:03 +0800

    remove privacy information

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import suffix
 for i in range(0,100):
-	print ((ID17 := "32058220010206" + str(i).zfill(2) + (sex := "2")) + suffix.suffix(ID17))#The first 6 numerals make up location code, next 8 determine which day he or she comes to the world.
+	print ((ID17 := "00000020000101" + str(i).zfill(2) + (sex := "2")) + suffix.suffix(ID17))#The first 6 numerals make up location code, next 8 determine which day he or she comes to the world.
 
